d82376046b0ffa6ee3a687406279855a5848fc99
Jhon Jairo Rojas Ortiz <jhonjairorojasortiz@gmail.com>
2022-05-24 17:41:37 -0500

    .

--- reports_integration/__manifest__.py
+++ reports_integration/__manifest__.py
@@ -8,7 +8,7 @@
     'license': 'LGPL-3',
     'category': '',
     'depends': [
-        'stock'
+        'stock_account', 'product'
     ],
     'data': [
         'views/stock_valuation_layer.xml',
